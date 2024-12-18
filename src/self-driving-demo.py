ed26a69042a2d7f9823ac332d58eaafd1114d97b
Yao Fehlis <yao@artificial.com>
2024-12-18 20:05:21 +0000

    added a TODO for later

--- src/self-driving-demo.py
+++ src/self-driving-demo.py
@@ -7,8 +7,10 @@ import yaml
 
 import utils, request_models
 
+# TODO: to clean up later: the datasets and the files grow as self-driving loop continues. Currently, the models use all files and data instead of 
+# the newly added ones for each iteration. It does unnecessary model inferences. I will clean this up later. 
 
-######## File paths ########
+################### FILE PATHS ###################
 with open("../configs/self-driving-demo.yaml", "r") as f:
     config = yaml.safe_load(f)
 
@@ -21,6 +23,7 @@ molmim_generated_csv = config['paths']['molmim_generated_csv']
 dsmbind_predictions_csv = config['paths']['dsmbind_predictions_csv']
 results_csv = config['paths']['results_csv']
 
+################### PREPARE FILES ###################
 utils.prepare_output_directory(diffdock_output_dir)
 utils.prepare_output_directory(dsmbind_input_dir)
 utils.delete_folders_in_folder(diffdock_output_dir)
@@ -36,6 +39,8 @@ folded_protein = utils.file_to_json_compatible_string(protein_file_path)
 # Get starting molecules
 df_starting_molecules = pd.read_csv(starting_molecule_csv)
 
+
+################### BASIC RUN: ROUND 0 ###################
 # Round 0
 print("Round 0")
 molecule_name = df_starting_molecules['Molecules'][0]
@@ -62,7 +67,7 @@ df_joined.to_csv('../data/results.csv')
 threshold_binding_affinity = config['threshold_binding_affinity']
 threshold_for_number_of_selected_molecules = config['threshold_for_number_of_selected_molecules']
 
-################# SELF-DRIVING LOOP STARTING ####################
+################### SELF-DRIVING LOOP STARTING ###################
 rd = 0
 while len(df_joined[df_joined['DSMBind_predictions'] < threshold_binding_affinity]) < threshold_for_number_of_selected_molecules:
     rd = rd + 1
